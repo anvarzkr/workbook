3bdc52f156af4f83228f75870eb1d7efe1517560
aydar0894 <aydar0894@mail.ru>
2017-11-10 13:58:00 +0300

    Web3

--- client/client.js
+++ client/client.js
@@ -5,7 +5,14 @@ import Web3 from 'web3'
 var contractInfo = require('../blockchain/build/contracts/EmploymentContract.json')
 var contract = require("truffle-contract")
 let Empl = contract(contractInfo)
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+if (typeof web3 !== 'undefined') {
+	console.log('Metamask initialized');
+	window.web3 = new Web3(web3.currentProvider);
+} else {
+	console.log('Metamask not initialized. Falling back to localhost:8545')
+	window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+}
+
 Empl.setProvider(web3.currentProvider)
 let emp = Empl.at("0xda13933f1a2cda9bcdb315ff138694074f56ae5c")
 
